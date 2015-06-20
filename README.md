e58c5e528d327cbbce618f45ca0076e457a37a4b
solvire <stevenjscott@gmail.com>
2015-06-20 16:30:18 -0700

    Initial Add

---
+++ README.md
@@ -0,0 +1,21 @@
+# Lykee CLI
+
+Command-Line Interface for the Lykee Application
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ lykee-cli --help
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Command-Line Interface for the Lykee Application
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='lykee-cli',
+    version='0.1.0',
+    url='https://github.com/solvire/lykee-cli',
+    license='BSD',
+    author='Lykee User',
+    author_email='lykeedev@gmail.com',
+    description='Command-Line Interface for the Lykee Application',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'lykee-cli = lykee_cli.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
